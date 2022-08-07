87665d1e19b62c6d1d19b91535301d6eee9c6acf
redeskako <carlosl.sanchez@gmail.com>
2022-08-07 17:02:35 +0200

    Añandiendo elementos EL y otros elementos

---
+++ J2EE/Basico/ejemploJSP/src/es/uned/master/java/Libro.java
@@ -0,0 +1,15 @@
+package com.prueba.jsp;
+
+public class Libro {
+	private String titulo;
+	
+	public Libro(){
+		
+	}
+	public String getTitulo(){
+		return this.titulo;
+	}
+	public void setTitulo(String titulo){
+		this.titulo= titulo;
+	}
+}
